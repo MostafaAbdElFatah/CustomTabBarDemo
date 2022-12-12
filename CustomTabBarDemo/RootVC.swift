82c967b1f24176cfa5e3db6592611a0826ab76c5
Mostafa AbdElFatah <m.abdelfattah@arabia-it.net>
2022-12-12 16:12:02 +0200

    Initial Commit

---
+++ CustomTabBarDemo/RootVC.swift
@@ -0,0 +1,19 @@
+//
+//  RootVC.swift
+//  CustomTabBarDemo
+//
+//  Created by Mostafa AbdElFatah on 12/12/2022.
+//
+
+import UIKit
+
+class RootVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
